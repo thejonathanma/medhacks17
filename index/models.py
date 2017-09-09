dd5a28585af9423d91e88d5baa07de63f528e0f2
Jonathan Ma <jonathan.ma@duke.edu>
2017-09-09 01:39:03 -0400

    connected psql

--- index/models.py
+++ index/models.py
@@ -4,3 +4,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Patient(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
--- medhacks17/settings.py
+++ medhacks17/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'medhacks17.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'test',
+        'USER': 'jonathan',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
